ce184f8ca138ba5438c967c4bbab22e3ef0c5e11
maria-gabriel <mariz.magr@gmail.com>
2024-01-06 19:05:26 -0600

    data connection and visualization in the custom table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, UsuariosModule
+    BrowserModule, HttpClientModule, UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usuarios/tabla-usuarios/tabla-usuarios.component.ts
+++ src/app/usuarios/tabla-usuarios/tabla-usuarios.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tabla-usuarios',
   templateUrl: './tabla-usuarios.component.html',
   styleUrls: ['./tabla-usuarios.component.css']
 })
-export class TablaUsuariosComponent {
+export class TablaUsuariosComponent implements OnInit {
+  usuarios: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:8000/api/usuarios').subscribe((data) => {
+      this.usuarios = data;
+      console.log(this.usuarios);
+    });
+  }
 }
